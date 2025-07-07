155e0ba616c2289bd1e5c6e0c7881a5d315f27ff
KuiChi <458114067@qq.com>
2025-07-07 09:18:05 +0800

    init

--- docker.sh
+++ docker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 sudo ubuntu-drivers autoinstall
+distribution=$(. /etc/os-release; echo $ID$VERSION_ID)  # e.g. ubuntu20.04
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+apt update && apt install -y nvidia-container-toolkit docker.io
+nvidia-smi
 
-apt update && apt intall -y nvidia-container-toolkit docker.io
 mkdir -p /var/py_workspace && cd /var/py_workspace
-git clone git@github.com:zhao458114067/xu_ai_assistant.git && cd xu_ai_assistant
+
+git clone https://github.com/zhao458114067/xu_ai_assistant.git && cd xu_ai_assistant
 
 docker build -t xu_ai_assistant .
 
