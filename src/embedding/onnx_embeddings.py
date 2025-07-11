bbd1b04524910f1905f342e32edc9072ff9893cf
KuiChi <458114067@qq.com>
2025-07-12 03:32:18 +0800

    init

--- src/embedding/onnx_embeddings.py
+++ src/embedding/onnx_embeddings.py
@@ -1,7 +1,6 @@
 import os
 import warnings
-import torch
-import torch.nn.functional as F
+import numpy as np
 from tqdm import tqdm
 import onnxruntime as ort
 from transformers import AutoTokenizer, AutoModel
@@ -12,11 +11,6 @@ from src.constants.constants import onnx_path, output_dir
 
 class OnnxEmbeddings(Embeddings):
     def __init__(self, model_name, device_type="auto"):
-        """
-        :param onnx_path: ONNX 文件路径或目录
-        :param model_name: HF 模型名称
-        :param device_type: auto / cuda / cpu / amd
-        """
         self.use_onnx = os.path.exists(onnx_path)
 
         if self.use_onnx:
@@ -25,6 +19,7 @@ class OnnxEmbeddings(Embeddings):
             self.tokenizer = AutoTokenizer.from_pretrained(output_dir, trust_remote_code=True)
 
         else:
+            import torch
             warnings.warn("未找到 ONNX 模型，将使用 Hugging Face PyTorch 模型，推理速度较慢")
             self.model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
             self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
@@ -64,24 +59,15 @@ class OnnxEmbeddings(Embeddings):
 
     def _embed(self, text):
         text = text.strip().replace("\n", " ")
-        tokens = self.tokenizer(text, return_tensors="pt" if not self.use_onnx else "np", padding="max_length",
+        tokens = self.tokenizer(text, return_tensors="np", padding="max_length",
                                 truncation=True, max_length=512)
 
-        if self.use_onnx:
-            # position_ids = np.arange(tokens["input_ids"].shape[1])[None, :].astype("int64")
-            ort_inputs = {
-                "input_ids": tokens["input_ids"],
-                "attention_mask": tokens["attention_mask"],
-                # "position_ids": position_ids,
-            }
-            outputs = self.session.run([self.output_name], ort_inputs)[0]
-            cls_embedding = outputs[:, 0, :]
-            normed = F.normalize(torch.tensor(cls_embedding), p=2, dim=1).numpy()
-            return normed[0].tolist()
-        else:
-            tokens = {k: v.to(self.device) for k, v in tokens.items()}
-            with torch.no_grad():
-                outputs = self.model(**tokens)
-            cls_embedding = outputs.last_hidden_state[:, 0, :]
-            normed = F.normalize(cls_embedding, p=2, dim=1).cpu().numpy()
-            return normed[0].tolist()
+        ort_inputs = {
+            "input_ids": tokens["input_ids"],
+            "attention_mask": tokens["attention_mask"],
+        }
+        outputs = self.session.run([self.output_name], ort_inputs)[0]
+        cls_embedding = outputs[:, 0, :]  # [batch, hidden]
+        norm = np.linalg.norm(cls_embedding, axis=1, keepdims=True)
+        normed = cls_embedding / (norm + 1e-10)
+        return normed[0].tolist()
